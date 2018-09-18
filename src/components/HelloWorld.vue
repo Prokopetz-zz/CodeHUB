243b69f2491d1ee39e0e68622ea4e888c7ec97c0
Nicolas Prokopetz <nicolas@prokopetz.com.br>
2018-09-18 15:44:28 -0300

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="menu">
+    <div>
+      Post
+    </div>
+    <div>
+      Home
+    </div>
+    <div>
+      Settings
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+.menu {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right:0;
+
+}
+
+
+</style>
