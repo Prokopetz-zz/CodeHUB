d4a96b60abdfdc44da58d8bb26ebfabc0dfe5112
Nicolas Prokopetz <nicolas@prokopetz.com.br>
2018-09-18 16:23:05 -0300

    Fix used vue

---
+++ src/components/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+    <m-menu-mobile/>
+  </div>
+</template>
+
+<script>
+import MMenuMobile from '@/components/m-menu-mobile';
+
+export default {
+  name: 'Index',
+  components: {
+    MMenuMobile,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
