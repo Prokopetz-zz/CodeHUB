adb83f331f56d96900e2054440ef75b4a7b5d0fe
joaodacolsoares <joao.dacol.soares@gmail.com>
2018-09-20 16:46:00 -0300

    Added atoms folder to the project

---
+++ src/components/atoms/a-text-box.vue
@@ -0,0 +1,23 @@
+<template>
+    <div :class="{'border' : border}">
+      <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ATextBox',
+  props: {
+    border: Boolean,
+  },
+};
+</script>
+
+<style lang='stylus' scoped>
+div{
+    padding: 20px 0;
+}
+.border {
+    border: 2px solid #4C3C1B;
+}
+</style>
