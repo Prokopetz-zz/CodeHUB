e36a99d6980658c9f21ffef7d674dbf25ce4a61a
joaodacolsoares <joao.dacol.soares@gmail.com>
2018-09-21 01:18:00 -0300

    Added more atoms -Icon atom -Text Input atom

---
+++ src/components/atoms/a-icon.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        icone
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AIcon',
+};
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
---
+++ src/components/atoms/a-input-text.vue
@@ -0,0 +1,42 @@
+<template>
+    <form>
+        <input id="input"
+               :placeholder="text"
+               @focus="focus"
+               @blur="blur">
+    </form>
+</template>
+
+<script>
+export default {
+  name: 'AInputText',
+  props: {
+    text: String,
+  },
+  methods: {
+    focus() {
+      document.getElementById('input').placeholder = '';
+    },
+    blur() {
+      document.getElementById('input').placeholder = this.text;
+    },
+  },
+};
+</script>
+
+<style lang='stylus' scoped>
+input {
+  padding: 1rem;
+  font-size: 17px;
+  border: none;
+  background: #fff;
+}
+form:after {
+  content: "";
+  clear: both;
+  border: none;
+}
+input:focus{
+    outline: none;
+}
+</style>
