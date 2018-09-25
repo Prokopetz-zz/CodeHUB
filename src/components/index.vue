c8e94b5c1a12a59d7b22e67d71739d1a5cd8bca7
joaodacolsoares <joao.dacol.soares@gmail.com>
2018-09-24 23:05:12 -0300

    Take your card Prokopetz

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="index">
     <o-header></o-header>
+    <a-card></a-card>
     <m-footer page="home"/>
   </div>
 </template>
 
 <script>
+import ACard from '@/components/atoms/a-card';
 import MFooter from '@/components/molecules/m-footer';
 import OHeader from '@/components/organisms/o-header';
 
 export default {
   name: 'Index',
   components: {
+    ACard,
     MFooter,
     OHeader,
   },
 };
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
 
 </style>
